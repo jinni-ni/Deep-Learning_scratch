2ed9897b3e9fb56deb47964a61a64129f89c9673
김수진 <sjkim910115@gmail.com>
2020-02-27 02:05:59 +0900

    01. perceptron 실습

---
+++ 01.perceptron.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def AND(x1,x2):
+    X = np.array([x1,x2])
+    W = np.array([0.5,0.5])
+    b = -0.7
+
+    tmp = np.sum(W*X) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1,x2):
+    X = np.array([x1,x2])
+    W = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(W*X) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1,x2):
+    X =np.array([x1,x2])
+    W = np.array([0.5,0.5])
+    b = -0.2
+    tmp = np.sum(W*X) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1,x2):
+    s1 = NAND(x1,x2)
+    s2 = OR(x1,x2)
+    y = AND(s1,s2)
+    return y
+
